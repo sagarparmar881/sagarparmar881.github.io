93462f4cb49df1e6586895b403513e4cbe6ae886
Sagar Parmar <47896458+sagarparmar881@users.noreply.github.com>
2023-04-23 21:05:30 +0530

    Initial Commit

---
+++ src/data/socialLinks.js
@@ -0,0 +1,38 @@
+export const socialLinks = [
+	{
+		id: 1,
+		name: 'Website',
+		icon: 'globe',
+		url: '#',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/sagarparmar881',
+	},
+	{
+		id: 3,
+		name: 'Twitter',
+		icon: 'twitter',
+		url: 'https://twitter.com/sagarparmar881',
+	},
+	{
+		id: 4,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/sagarparmar881',
+	},
+	{
+		id: 5,
+		name: 'Instagram',
+		icon: 'instagram',
+		url: 'https://www.instagram.com/saaagar.arts/',
+	},
+	{
+		id: 6,
+		name: 'HackerRank',
+		icon: 'code',
+		url: 'https://www.hackerrank.com/sagarparmar881',
+	}
+];
